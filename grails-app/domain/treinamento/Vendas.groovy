a6b3bf32516fb21b2d13d89b742f1eca1e224303
GustMon404 <gustavomonteiro782@gmail.com>
2020-11-27 13:55:35 -0300

    Adicionando funcoes em Item

--- grails-app/domain/treinamento/Vendas.groovy
+++ grails-app/domain/treinamento/Vendas.groovy
@@ -4,9 +4,19 @@ class Vendas {
     Date data;
     Date dataConfirmacao;
     Date dataCancelamento;
-
+    String situacao = "Cadastrada";
     Cliente cliente;
-    /*Item itens = []*/
+
+    def confirmar(){
+        this.situacao = "Confirmada"
+        this.dataConfirmacao = new Date()
+    }
+
+    def cancelar(){
+        this.situacao = "Cancelada"
+        this.dataCancelamento = new Date()
+    }
+
     static constraints = {
     }
 }
